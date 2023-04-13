45c86db38583eb63c51b6816775061a9e9eec057
Karatsupa <andreykaratsupa@gmail.com>
2023-04-13 21:09:44 +0300

    add Jacoco and Test100

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,2 +1,31 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+
 public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void findMax100() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 25, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 25;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
